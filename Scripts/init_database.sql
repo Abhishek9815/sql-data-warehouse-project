9d0aefa9f3edd3c09abae8dda9d3ad15bffa4146
Abhishek9815 <102174599+Abhishek9815@users.noreply.github.com>
2026-05-13 18:07:50 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -13,14 +13,14 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
-USE master;
-GO
+USE master;        --Switch context to Master databse, Master is a system database in SQL server, user and server level operation 
+GO                 --It acts like a seperator between batches of SQl commands
 
 -- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')            --  sys.database is a system table containing all database info.
+BEGIN                                                                           -- SELECT 1 is used only for existance checking for better performance.     
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;       --force database into single_user mode
+    DROP DATABASE DataWarehouse;                                                --immediately disconnects all active users
 END;
 GO
 
